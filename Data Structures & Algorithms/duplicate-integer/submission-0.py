7d655b1aab7d12bfafe3b8e194f7cf60de8cc360
parthsvault <parthsvault@gmail.com>
2026-07-01 17:16:36 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        count = []
+
+        for i in nums:
+            if i in count:
+                return True
+            count.append(i)
+
+        return False
