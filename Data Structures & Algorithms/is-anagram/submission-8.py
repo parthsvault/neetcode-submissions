9623c4cb809efd9c9cbeaa32baad02a040375bea
parthsvault <parthsvault@gmail.com>
2026-07-01 17:30:23 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+
+        def counter(inp):
+            count = {}
+            for char in inp:
+                count[char] = count.get(char, 0) + 1
+           
+            return count
+
+        s_map = counter(s)
+        t_map = counter(t)
+
+        if s_map == t_map: return True 
+        else: return False
