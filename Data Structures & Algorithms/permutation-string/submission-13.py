e50e91f6a189d3269dfe4a79228e7ec579ac26bc
parthsvault <parthsvault@gmail.com>
2026-08-08 00:43:57 +0530

    Add: permutation-string - submission-13

---
+++ Data Structures & Algorithms/permutation-string/submission-13.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool: 
+        count = {}                                      #first on my own dsa med problem
+        for i in s1:
+            count[i] = count.get(i, 0) + 1
+        print(count)
+        l = 0
+        r = 0
+        while r < len(s2):
+            if s2[r] in count and count[s2[r]] > 0:
+                count[s2[r]] -= 1
+                if set(count.values()) == {0}:
+                    return(True)
+            else:
+                while l < r:
+                    if s2[l] in count:
+                        count[s2[l]] += 1
+                        l += 1
+                        r -= 1
+                        break
+                    l += 1
+            r += 1
+
+        return(False)
+
